ad3d77ad318f7f5dcb31ec20621526d79c11c594
SumSumSoo <132228906+SumSumSoo@users.noreply.github.com>
2026-01-15 16:41:50 +0000

    Fixed an error

--- anythink-market-server/src/index.js
+++ anythink-market-server/src/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = 8001;
 
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to Anythink Market Server' });
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
